73c634fb14519b9ba908d85097dc2931df306b34
lacsinagramar <lacsinagramar@gmail.com>
2018-04-22 17:12:00 +0800

    links fix

--- app/modules/management/routes.js
+++ app/modules/management/routes.js
@@ -9,14 +9,14 @@ router.get('/consignment', (req, res) => {
 	res.render('management/views/consignment')
 });
 
+router.get('/auction', (req, res) => {
+	res.render('management/views/auctionSchedule')
+});
 
 //Router for 404 Page
 router.get('*', (req, res) => {
 	console.log('404 Page')
 	res.status(404).render('management/views/404');
 })
-router.get('/auction', (req, res) => {
-	res.render('management/views/auctionSchedule')
-});
 
 exports.index = router
