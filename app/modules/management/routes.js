e7eb32526ee67cb8b1c72c31d37d2b0bda4743bf
lacsinagramar <lacsinagramar@gmail.com>
2018-04-22 16:51:12 +0800

    Calendar

--- app/modules/management/routes.js
+++ app/modules/management/routes.js
@@ -9,4 +9,8 @@ router.get('/consignment', (req, res) => {
 	res.render('management/views/consignment')
 });
 
+router.get('/auction', (req, res) => {
+	res.render('management/views/auctionSchedule')
+});
+
 exports.index = router
