217231e2bbbe02d433615f9ae45864a57bcd511f
lacsinagramar <lacsinagramar@gmail.com>
2018-04-24 02:44:25 +0800

    Auction Sched Query

--- app/modules/management/routes.js
+++ app/modules/management/routes.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var db = require('../../lib/database')();
 
 router.get('/', (req, res) => {
 	res.render('management/views/index')
@@ -10,7 +12,22 @@ router.get('/consignment', (req, res) => {
 });
 
 router.get('/auction', (req, res) => {
-	res.render('management/views/auctionSchedule')
+	var queryString = `SELECT * FROM tbl_auction`;
+	var events = [];
+	db.query(queryString, (err, results, fields) =>{
+		if(err) console.log(err)
+		console.log(results);
+
+		for(var i=0;i<results.length;i++){
+			events.push({});
+			events[i].title = 'Auction #'+results[i].intAuctionID;
+			events[i].start = moment(results[i].datDateStart).format('YYYY-MM-DD HH:mm');
+			events[i].end = moment(results[i].datDateEnd).format('YYYY-MM-DD HH:mm');
+		}
+
+
+		return res.render('management/views/auctionSchedule', {auctionSched: events});
+	});
 });
 
 //Router for 404 Page
