6a6a0080c3c06e64a50fac78920d3274976af6b0
lacsinagramar <lacsinagramar@gmail.com>
2018-04-26 18:07:13 +0800

    Auction Sched Querying

--- app/modules/management/routes.js
+++ app/modules/management/routes.js
@@ -19,14 +19,25 @@ router.get('/auction', (req, res) => {
 		console.log(results);
 
 		for(var i=0;i<results.length;i++){
-			events.push({});
-			events[i].title = 'Auction #'+results[i].intAuctionID;
-			events[i].start = moment(results[i].datDateStart).format('YYYY-MM-DD HH:mm');
-			events[i].end = moment(results[i].datDateEnd).format('YYYY-MM-DD HH:mm');
+			results[i].jsonDuration = JSON.parse(results[i].jsonDuration)
+			if(results[i].booAuctionType != 0){
+				events.push({});
+				events[i].id = results[i].intAuctionID;
+				events[i].title = 'Auction #'+results[i].intAuctionID;
+				events[i].start = moment(results[i].datDateStart).format('YYYY-MM-DD HH:mm');
+				var endDate = moment(events[i].start).add(results[i].jsonDuration.days, 'd').add(results[i].jsonDuration.hours, 'h').format('YYYY-MM-DD HH:mm');
+				events[i].end = endDate;
+				if(results[i].booAuctionType == 1)
+					events[i].color = '#122d59'
+				else if (results[i].booAuctionType == 2)
+					events[i].color = '#591212'
+				//events[i].editable = false;
+				results.splice(i,1)
+			}
 		}
 
-
-		return res.render('management/views/auctionSchedule', {auctionSched: events});
+		console.log(results);
+		return res.render('management/views/auctionSchedule', {auctionSched: events, unscheduled: results});
 	});
 });
 
@@ -34,6 +45,14 @@ router.get('/auction', (req, res) => {
 router.get('*', (req, res) => {
 	console.log('404 Page')
 	res.status(404).render('management/views/404');
+})
+
+
+//AJAX 
+router.post('/eventDetails', (req, res) => {
+	db.query('SELECT * FROM tbl_auction WHERE intAuctionID = ?', [req.body.auctionId], (err, results, fields) =>{
+		res.send(results[0]);
+	});
 })
 
 exports.index = router
