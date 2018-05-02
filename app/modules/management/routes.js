fdcf0c02a5c81255ed30243c3dac448b39cdc50b
lacsinagramar <lacsinagramar@gmail.com>
2018-05-02 19:23:08 +0800

    edit auction details drag and drop and inside details and bug fixes

--- app/modules/management/routes.js
+++ app/modules/management/routes.js
@@ -17,30 +17,34 @@ router.get('/auction', (req, res) => {
 	db.query(queryString, (err, results, fields) =>{
 		if(err) console.log(err)
 		console.log(results);
+		var unscheduledEvents = results;
 
 		for(var i=0;i<results.length;i++){
 			if(results[i].booAuctionStatus != 0){
 				results[i].jsonDuration = JSON.parse(results[i].jsonDuration)
-				events.push({});
-				events[i].id = results[i].intAuctionID;
-				events[i].title = 'Auction #'+results[i].intAuctionID;
-				events[i].start = moment(results[i].datDateStart).format('YYYY-MM-DD HH:mm');
-				var endDate = moment(events[i].start).add(results[i].jsonDuration.days, 'd').add(results[i].jsonDuration.hours, 'h').add(results[i].jsonDuration.minutes, 'm').format('YYYY-MM-DD HH:mm');
-				events[i].end = endDate;
+				var event = {};
+				event.id = results[i].intAuctionID;
+				event.title = 'Auction #'+results[i].intAuctionID;
+				event.start = moment(results[i].datDateStart).format('YYYY-MM-DD HH:mm');
+				var endDate = moment(event.start).add(results[i].jsonDuration.days, 'd').add(results[i].jsonDuration.hours, 'h').add(results[i].jsonDuration.minutes, 'm').format('YYYY-MM-DD HH:mm');
+				event.end = endDate;
 				if(results[i].booAuctionType == 1)
-					events[i].color = '#122d59'
+					event.color = '#122d59'
 				else if (results[i].booAuctionType == 2)
-					events[i].color = '#591212'
+					event.color = '#591212'
 				if(results[i].booAuctionStatus == 1)
-					events[i].editable = true;
-				else if (results[i].booAuctionStatus == 2)
-					events[i].editable = false;
-				results.splice(i,1)
+					event.editable = true;
+				else if (results[i].booAuctionStatus == 2 || results[i].booAuctionStatus == 3)
+					event.editable = false;
+				events.push(event);
+
+				unscheduledEvents = unscheduledEvents.filter(item => item.intAuctionID != i+1);
 			}
 		}
 
-		console.log(results);
-		return res.render('management/views/auctionSchedule', {auctionSched: events, unscheduled: results});
+		console.log('\nNASA CATALOG\n');
+		console.log(unscheduledEvents)
+		return res.render('management/views/auctionSchedule', {auctionSched: events, unscheduled: unscheduledEvents});
 	});
 });
 
@@ -78,6 +82,30 @@ router.post('/scheduleAuction', (req, res) => {
 		if(err) console.log(err)
 		return res.send(true)
 	});
+});
+
+/* editType
+1 - update inside details including datDateStart(TIME)
+2 - datDateStart(DATE) 
+*/
+router.post('/editAuctionDetails', (req, res) => {
+	var queryString;
+	if(req.body.editType == 1){
+		queryString = `UPDATE tbl_auction SET datDateStart=?, booAuctionType=?, jsonDuration=? WHERE intAuctionID=?`;
+		db.query(queryString,[req.body.datDateStart,req.body.booAuctionType,req.body.jsonDuration,req.body.intAuctionID], (err, results, fields) => {
+			if(err) console.log(err)
+
+			return res.send(true)
+		});
+	}
+	if(req.body.editType == 2){
+		queryString = `UPDATE tbl_auction SET datDateStart=? WHERE intAuctionID=?`;
+		db.query(queryString, [req.body.datDateStart, req.body.intAuctionID], (err, results, fields) => {
+			if(err) console.log(err)
+
+			return res.send(true)
+		})
+	}
 });
 
 router.post('/refetchEvents', (req, res) => {
